75721752845eadfdff62d25f62dd5f51c3fb562f
Fikri Ramadianto <ramadiantofikri@gmail.com>
2022-12-30 00:06:18 +0700

    final

--- handler.js
+++ handler.js
@@ -7,6 +7,26 @@ const addBook = (request, h) => {
     name, year, author, summary, publisher, pageCount, readPage, reading,
   } = request.payload;
 
+  if (name === undefined || name === null) {
+
+    const response = h.response({
+      status: 'fail',
+      message: 'Gagal menambahkan buku. Mohon isi nama buku',
+    });
+    response.code(400);
+    return response;
+  }
+
+  if (readPage > pageCount) {
+    const response = h.response({
+      status: 'fail',
+      message: 'Gagal menambahkan buku. readPage tidak boleh lebih besar dari pageCount',
+    });
+    response.code(400);
+    return response;
+  }
+
+
   const id = nanoid(16);
 
   const insertedAt = new Date().toISOString();
@@ -29,25 +49,6 @@ const addBook = (request, h) => {
   };
 
   books.push(newBook);
-
-  if (name === undefined || name === null) {
-
-    const response = h.response({
-      status: 'fail',
-      message: 'Gagal menambahkan buku. Mohon isi nama buku',
-    });
-    response.code(400);
-    return response;
-  }
-
-  if (readPage > pageCount) {
-    const response = h.response({
-      status: 'fail',
-      message: 'Gagal menambahkan buku. readPage tidak boleh lebih besar dari pageCount',
-    });
-    response.code(400);
-    return response;
-  }
 
   const isSuccess = books.filter((book) => book.id === id).length > 0;
 
